7af4254ea2b551bccb0a2e361ef3b932088aab54
ftuyama <felipetuyama@gmail.com>
2017-11-04 16:51:50 -0200

    Adds one more sensor

--- routes/data.js
+++ routes/data.js
@@ -4,7 +4,7 @@ var sqlite3 = require('sqlite3').verbose();
 var db = new sqlite3.Database('database');
 
 db.serialize(function() {
-  db.run("CREATE TABLE IF NOT EXISTS data (v1 REAL, v2 REAL, v3 REAL, time DATETIME, t DATETIME DEFAULT CURRENT_TIMESTAMP)");
+  db.run("CREATE TABLE IF NOT EXISTS data (v1 REAL, v2 REAL, v3 REAL, v4 REAL, time DATETIME, t DATETIME DEFAULT CURRENT_TIMESTAMP)");
 });
 
 /* Add new data */
@@ -12,9 +12,9 @@ router.post('/', function(req, res) {
   //console.log(req.body);
   data = req.body.rows;
   db.serialize(function() {
-    var stmt = db.prepare("INSERT INTO data (v1, v2, v3, time) VALUES (?, ?, ?, ?)");
+    var stmt = db.prepare("INSERT INTO data (v1, v2, v3, v4, time) VALUES (?, ?, ?, ?, ?)");
     data.forEach(function(row, i, arr) {
-      stmt.run([row.v1, row.v2, row.v3, row.time]);
+      stmt.run([row.v1, row.v2, row.v3, row.v4, row.time]);
     });
     stmt.finalize();
   });
@@ -32,6 +32,7 @@ router.get('/', function(req, res) {
         'v1': row.v1,
         'v2': row.v2,
         'v3': row.v3,
+        'v4': row.v4,
       });
     }, function() {
       res.send(data);
@@ -41,7 +42,7 @@ router.get('/', function(req, res) {
 
 router.get('/reset', function(req, res) {
   db.run("DROP TABLE data");
-  db.run("CREATE TABLE IF NOT EXISTS data (v1 REAL, v2 REAL, v3 REAL, time DATETIME, t DATETIME DEFAULT CURRENT_TIMESTAMP)");
+  db.run("CREATE TABLE IF NOT EXISTS data (v1 REAL, v2 REAL, v3 REAL, v4 REAL, time DATETIME, t DATETIME DEFAULT CURRENT_TIMESTAMP)");
   res.send('ok');
 });
 
